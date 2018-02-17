956c0125d2ad2ad20b16538f7404a7c3816b1333
Captain ALM <32275498+Captain-ALM@users.noreply.github.com>
2018-02-17 08:02:23 +0000

    Fixed a small textbox bug.
    Fixed the keydown event handler for txtbxlog not being registered.

--- calmcon/main.vb
+++ calmcon/main.vb
@@ -333,7 +333,7 @@ Public Class main
         End If
     End Sub
 
-    Private Sub txtbxlog_KeyDown(sender As Object, e As KeyEventArgs)
+    Private Sub txtbxlog_KeyDown(sender As Object, e As KeyEventArgs) Handles txtbxlog.KeyDown
         If (e.KeyCode = Keys.A And e.Control And Not txtbxlog.SelectionLength >= txtbxlog.Text.Length) Then
             e.SuppressKeyPress = True
             txtbxlog.SelectAll()
